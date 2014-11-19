0808bcd6b50e88c01a249c129fe259df9cc61ad6
LiamAttClarke <liam.att.clarke@gmail.com>
2014-11-19 01:03:30 -0500

    First Commit

---
+++ Assets/Scripts/Map.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Map : MonoBehaviour {
+	GameObject tile;
+	void GenerateTerrain () {
+		GameObject tileClone = (GameObject)Instantiate (tile);
+		int ringScope = 2;
+		int ringCurrent = 1;
+		float apothem;
+		int apothemMultiplyer = 2;
+		float tileAngle = 30;
+		float tileAngleIncrement = 60;
+		// rings
+		for (int i = 0; i < ringScope; i++) {
+			// tiles for each ring
+			for (int j = 0; j < 6 * ringCurrent; j++) {
+				apothem = Mathf.Cos(30 * Mathf.PI / 180) * apothemMultiplyer;
+				tileClone = (GameObject)Instantiate (
+					tile, 
+					new Vector3 (apothem * Mathf.Cos(tileAngle * Mathf.PI / 180), 0, apothem * Mathf.Sin(tileAngle * Mathf.PI / 180)),
+					Quaternion.identity);
+				tileAngle += tileAngleIncrement;
+			}
+			ringCurrent++;
+			tileAngleIncrement /= 2;
+			apothemMultiplyer += 2;
+		}
+
+	}
+	void Start () {
+		tile = (GameObject)Resources.Load ("Prefabs/Tile");
+		GenerateTerrain ();
+	}
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/Tile.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tile : MonoBehaviour {
+	void CreateTile () {
+		// Add Components
+		MeshFilter tileMeshFilter = gameObject.AddComponent<MeshFilter> ();
+		gameObject.AddComponent<MeshRenderer> ();
+		// Create Mesh
+		/*     3 -- 2   radius = 1
+		 *   /   |   \
+		 *  4 -- 0 -- 1
+		 *   \   |   /
+		 *    5 --- 6
+		 */ 
+		Mesh tileMesh = new Mesh();
+		// mesh.vertices
+		float componentY = Mathf.Sin (60 * Mathf.PI / 180);
+		Vector3[] vertices = new Vector3[] {
+			new Vector3 (0, 0, 0), // 0
+			new Vector3 (1, 0, 0), // 1
+			new Vector3 (0.5f, componentY, 0), // 2
+			new Vector3 (-0.5f, componentY, 0), // 3
+			new Vector3 (-1, 0, 0), // 4
+			new Vector3 (-0.5f, -componentY, 0), // 5
+			new Vector3 (0.5f, -componentY, 0) // 6
+		};
+		// mesh.triangles
+		int[] triangles = new int[] {
+			0,2,1, // 1
+			0,3,2, // 2
+			0,4,3, // 3
+			0,5,4, // 4
+			0,6,5, // 5
+			0,1,6  // 6
+		};
+		//mesh.uv
+		Vector2[] uv = new Vector2[] {
+			new Vector2 (0.25f, 0), // 0
+			new Vector2 (0.75f, 0), // 1
+			new Vector2 (0, 0.5f), // 2
+			new Vector2 (0.5f, 0.5f), // 3
+			new Vector2 (1, 0.5f), // 4
+			new Vector2 (0.25f, 1), // 5
+			new Vector2 (0.75f, 1) // 6
+		};
+		// Set mesh
+		tileMeshFilter.mesh.vertices = vertices;
+		tileMeshFilter.mesh.triangles = triangles;
+		tileMeshFilter.mesh.uv = uv;
+	}
+	void Start () {
+		// Create tile
+		CreateTile();
+		// Orient tile
+		transform.Rotate (new Vector3 (90,0,0));
+	}
+}
