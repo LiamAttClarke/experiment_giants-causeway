36b14886e312f4a99c009e43e21796f79e35c8ce
LiamAttClarke <liam.att.clarke@gmail.com>
2014-11-25 20:33:55 -0500

    Added Randomized Map Generation
    Fixed hexagonal grid, Added map generation using Perlin Noise

--- Assets/Scripts/Map.cs
+++ Assets/Scripts/Map.cs
@@ -4,29 +4,29 @@ using System.Collections;
 public class Map : MonoBehaviour {
 	GameObject tile;
 	void GenerateTerrain () {
-		GameObject tileClone = (GameObject)Instantiate (tile);
-		int ringScope = 2;
-		int ringCurrent = 1;
-		float apothem;
-		int apothemMultiplyer = 2;
-		float tileAngle = 30;
-		float tileAngleIncrement = 60;
-		// rings
-		for (int i = 0; i < ringScope; i++) {
-			// tiles for each ring
-			for (int j = 0; j < 6 * ringCurrent; j++) {
-				apothem = Mathf.Cos(30 * Mathf.PI / 180) * apothemMultiplyer;
-				tileClone = (GameObject)Instantiate (
-					tile, 
-					new Vector3 (apothem * Mathf.Cos(tileAngle * Mathf.PI / 180), 0, apothem * Mathf.Sin(tileAngle * Mathf.PI / 180)),
-					Quaternion.identity);
-				tileAngle += tileAngleIncrement;
+		int mapSize = 32;
+		float apothem = Mathf.Cos(30 * Mathf.PI / 180);
+		int diameter = 2;
+		float offsetZ;
+		float offsetX = 0.5f;
+		float scale = 6.5f;
+		float amplitude = 10.0f;
+		float random = Random.Range (0, 100);
+		for (int x = 0; x < mapSize; x++) {
+			for (int z = 0; z < mapSize; z++) {
+				if (x % 2 != 0) {
+					offsetZ = apothem;
+				} else {
+					offsetZ = 0;
+				}
+				float height = Mathf.PerlinNoise (random + x / scale, random + z / scale);
+				GameObject tileClone = (GameObject)Instantiate (
+					tile,
+					new Vector3 (x * diameter - (offsetX * x), Mathf.Round (height * amplitude), (z * apothem) * diameter + offsetZ),
+					Quaternion.identity
+				);
 			}
-			ringCurrent++;
-			tileAngleIncrement /= 2;
-			apothemMultiplyer += 2;
 		}
-
 	}
 	void Start () {
 		tile = (GameObject)Resources.Load ("Prefabs/Tile");
