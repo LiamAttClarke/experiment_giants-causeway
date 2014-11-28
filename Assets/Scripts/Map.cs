b6c437fc199ee523e477bb77bf16c5dce84bfb90
LiamAttClarke <liam.att.clarke@gmail.com>
2014-11-27 21:33:40 -0500

    Fixed Tiles
    Optimized triangles, Split up mesh vertices along UV seams, Fixed UV
    coordinates, Applied temporary texture to tiles

--- Assets/Scripts/Map.cs
+++ Assets/Scripts/Map.cs
@@ -5,43 +5,51 @@ public class Map : MonoBehaviour {
 	GameObject tile;
 	float random;
 	float height;
+	GameObject[] tileArray;
 	public int mapSize = 16;
 	public float scale = 10.0f;
 	public float amplitude = 5.0f;
 	public float translate_x = 0;
 	public float translate_z = 0;
-	void GenerateTerrain () {
-		float apothem = Mathf.Cos(30 * Mathf.PI / 180);
-		int diameter = 2;
-		float offsetZ;
-		float offsetX = 0.5f;
-		random = Random.Range (0, 100);
-		for (int x = 0; x < mapSize; x++) {
-			for (int z = 0; z < mapSize; z++) {
-				if (x % 2 != 0) {
-					offsetZ = apothem;
-				} else {
-					offsetZ = 0;
-				}
-				height = Mathf.PerlinNoise (x / scale + random, z / scale + random);
-				GameObject tileClone = (GameObject)Instantiate (
-					tile, 
-					new Vector3 (x * diameter - (offsetX * x), height * amplitude, (z * apothem) * diameter + offsetZ), 
-					Quaternion.identity
-				);
-			}
-		}
-	}
 	void Start () {
 		tile = (GameObject)Resources.Load ("Prefabs/Tile");
 		GenerateTerrain ();
+		tileArray = GameObject.FindGameObjectsWithTag ("Tile");
 	}
 	void Update () {
-		GameObject[] tileArray = GameObject.FindGameObjectsWithTag ("Tile");
+		ModulateTerrain();
+	}
+	void GenerateTerrain () {
+		float radius = 0.5f;
+		float apothem = radius * Mathf.Sin (60 * Mathf.PI / 180);
+		float offset_z;
+		float offset_x = 0.25f;
+		random = Random.Range (0, 100);
+		// Generate hexagonal grid
+		for (int x = 0; x < mapSize; x++) {
+			for (int z = 0; z < mapSize; z++) {
+				// For every odd row, offset by apothem along z
+				if (x % 2 != 0) {
+					offset_z = apothem;
+				} else {
+					offset_z = 0;
+				}
+				// Set height of tile
+				height = Mathf.PerlinNoise (x / scale + random, z / scale + random);
+				GameObject tileClone = (GameObject)Instantiate (
+					tile, 
+					new Vector3 (x - (offset_x * x), height * amplitude, (z * apothem) * 2 + offset_z), 
+					Quaternion.identity
+					);
+				// Parent tile to Map gameObject
+				tileClone.transform.parent = GameObject.Find ("Map").transform;
+			}
+		}
+	}
+	void ModulateTerrain () {
 		foreach (GameObject tile in tileArray) {
 			height = Mathf.PerlinNoise ((tile.transform.position.x / 2) / scale + random + translate_x, (tile.transform.position.z / 2) / scale + random + translate_z);
 			tile.transform.position = new Vector3 (tile.transform.position.x, height * amplitude, tile.transform.position.z);
 		}
-		translate_x += 0.25f;
 	}
 }
--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -4,96 +4,111 @@ using System.Collections;
 public class Tile : MonoBehaviour {
 	void CreateTile () {
 		// Add Components
-		MeshFilter tileMeshFilter = gameObject.AddComponent<MeshFilter> ();
+		Mesh tileMesh = gameObject.AddComponent<MeshFilter> ().mesh;
 		gameObject.AddComponent<MeshRenderer> ();
 		// Create Mesh
-		/*     3 -- 2   radius = 1
-		 *   /   |   \
-		 *  4 -- 0 -- 1
-		 *   \   |   /
-		 *    5 --- 6
-		 * 
-		 *     10 -- 9
-		 *   /   |   \
-		 *  11 -- 7 -- 8
-		 *   \   |   /
-		 *    12 --- 13
+		/*       TOP                 SIDEx4                SIDEx2
+		 *     3 -- 2        7 -- 8 -- 9 -- 10-- 11     17-- 18-- 19
+		 *   / |    |\       |    |    |    |    |      |    |    |
+		 *  4  |    | 1      |    |    |    |    |      |    |    |
+		 *  |  |    |/|      |    |    |    |    |      |    |    |
+		 *  | 5 --- 6 |      |    |    |    |    |      |    |    |
+		 *  | |* -- | |      |    |    |    |    |      |    |    |
+		 *  |/|     |\|      |    |    |    |    |      |    |    |
+		 *  * |     | *      |    |    |    |    |      |    |    |
+		 *   \|     |/       |    |    |    |    |      |    |    |
+		 *    * --- *        12-- 13-- 14-- 15-- 16    20 -- 21-- 22
 		 */ 
-		Mesh tileMesh = new Mesh();
 		// mesh.vertices
-		float componentY = Mathf.Sin (60 * Mathf.PI / 180);
-		Vector3[] vertices = new Vector3[] {
-			// Front
-			new Vector3 (0, 0, 0), // 0
-			new Vector3 (1, 0, 0), // 1
-			new Vector3 (0.5f, componentY, 0), // 2
-			new Vector3 (-0.5f, componentY, 0), // 3
-			new Vector3 (-1, 0, 0), // 4
-			new Vector3 (-0.5f, -componentY, 0), // 5
-			new Vector3 (0.5f, -componentY, 0), // 6
-			// Back
-			new Vector3 (0, 0, 1), // 7
-			new Vector3 (1, 0, 1), // 8
-			new Vector3 (0.5f, componentY, 1), // 9
-			new Vector3 (-0.5f, componentY, 1), // 10
-			new Vector3 (-1, 0, 1), // 11
-			new Vector3 (-0.5f, -componentY, 1), // 12
-			new Vector3 (0.5f, -componentY, 1) // 13
+		float radius = 0.5f;
+		float apothem = radius * Mathf.Sin (60 * Mathf.PI / 180);
+		float component_x = radius * Mathf.Cos (60 * Mathf.PI / 180);
+		tileMesh.vertices = new Vector3[] {
+			// TOP
+			/*1*/new Vector3 (radius, radius, 0),
+			/*2*/new Vector3 (component_x, radius, apothem),
+			/*3*/new Vector3 (-component_x, radius, apothem),
+			/*4*/new Vector3 (-radius, radius, 0),
+			/*5*/new Vector3 (-component_x, radius, -apothem),
+			/*6*/new Vector3 (component_x, radius, -apothem),
+			// SIDEx4
+			/*7*/new Vector3 (radius, radius, 0),
+			/*8*/new Vector3 (component_x, radius, apothem),
+			/*9*/new Vector3 (-component_x, radius, apothem),
+			/*10*/new Vector3 (-radius, radius, 0),
+			/*11*/new Vector3 (-component_x, radius, -apothem),
+			/*12*/new Vector3 (radius, -radius, 0),
+			/*13*/new Vector3 (component_x, -radius, apothem),
+			/*14*/new Vector3 (-component_x, -radius, apothem),
+			/*15*/new Vector3 (-radius, -radius, 0),
+			/*16*/new Vector3 (-component_x, -radius, -apothem),
+			//SIDEx2
+			/*17*/new Vector3 (-component_x, radius, -apothem),
+			/*18*/new Vector3 (component_x, radius, -apothem),
+			/*19*/new Vector3 (radius, radius, 0),
+			/*20*/new Vector3 (-component_x, -radius, -apothem),
+			/*21*/new Vector3 (component_x, -radius, -apothem),
+			/*22*/new Vector3 (radius, -radius, 0),
 		};
 		// mesh.triangles
-		int[] triangles = new int[] {
-			// Front
-			0,2,1, // 1
-			0,3,2, // 2
-			0,4,3, // 3
-			0,5,4, // 4
-			0,6,5, // 5
-			0,1,6,  // 6
-//			// Back
-//			7,8,9,
-//			7,9,10,
-//			7,10,11,
-//			7,11,12,
-//			7,12,13,
-//			7,13,8,
-			// Sides
-			1,2,9,
-			1,9,8,
-			2,3,10,
-			2,10,9,
-			3,4,11,
-			3,11,10,
-			4,5,12,
-			4,12,11,
-			5,6,13,
-			5,13,12,
-			6,1,8,
-			6,8,13
+		tileMesh.triangles = new int[] {
+			// TOP
+			0,5,4,
+			0,4,3,
+			0,3,2,
+			0,2,1,
+			// SIDEx4
+			6,7,11,
+			7,12,11,
+			7,8,12,
+			8,13,12,
+			8,9,13,
+			9,14,13,
+			9,10,14,
+			10,15,14,
+			// SIDEx2
+			16,17,19,
+			17,20,19,
+			17,18,20,
+			18,21,20
 		};
+		float radiusUV = 0.25f;
+		float apothemUV = radiusUV * Mathf.Sin (60 * Mathf.PI /180);
+		float componentUV_x = apothemUV * Mathf.Tan (30 * Mathf.PI /180);
 		//mesh.uv
-		Vector2[] uv = new Vector2[] {
-			new Vector2 (0.25f, 0), // 0
-			new Vector2 (0.75f, 0), // 1
-			new Vector2 (0, 0.5f), // 2
-			new Vector2 (0.5f, 0.5f), // 3
-			new Vector2 (1, 0.5f), // 4
-			new Vector2 (0.25f, 1), // 5
-			new Vector2 (0.75f, 1) // 6
+		tileMesh.uv = new Vector2[] {
+			// From L -> R && B -> T
+			/*1*/new Vector2 (1, apothemUV),
+			/*2*/new Vector2 (1 - componentUV_x, apothemUV * 2),
+			/*3*/new Vector2 (0.5f + componentUV_x, apothemUV * 2),
+			/*4*/new Vector2 (0.5f, apothemUV),
+			/*5*/new Vector2 (0.5f + componentUV_x, 0),
+			/*6*/new Vector2 (1 - componentUV_x, 0),
+			/*7*/new Vector2 (0, 1),
+			/*8*/new Vector2 (0.25f, 1),
+			/*9*/new Vector2 (0.5f, 1),
+			/*10*/new Vector2 (0.75f, 1),
+			/*11*/new Vector2 (1, 1),
+			/*12*/new Vector2 (0, 0.5f),
+			/*13*/new Vector2 (0.25f, 0.5f),
+			/*14*/new Vector2 (0.5f, 0.5f),
+			/*15*/new Vector2 (0.75f, 0.5f),
+			/*16*/new Vector2 (1, 0.5f),
+			/*17*/new Vector2 (0, 0.5f),
+			/*18*/new Vector2 (0.25f, 0.5f),
+			/*19*/new Vector2 (0.5f, 0.5f),
+			/*20*/new Vector2 (0, 0),
+			/*21*/new Vector2 (0.25f, 0),
+			/*22*/new Vector2 (0.5f, 0)
 		};
-		// Set mesh
-		tileMeshFilter.mesh.vertices = vertices;
-		tileMeshFilter.mesh.triangles = triangles;
-		tileMeshFilter.mesh.uv = uv;
-		tileMeshFilter.mesh.RecalculateNormals ();
-		tileMeshFilter.mesh.Optimize(); 
-		Material ground = (Material)Resources.Load("Material/Ground");
+		tileMesh.RecalculateNormals ();
+		tileMesh.Optimize(); 
+		Material ground = (Material)Resources.Load("Materials/Ground");
 		renderer.material = ground;
 	}
 	void Start () {
 		// Create tile
 		CreateTile();
 		// Orient tile
-		transform.Rotate (new Vector3 (90,0,0));
-		transform.localScale = new Vector3 (transform.localScale.x, transform.localScale.y, 6);
 	}
 }
